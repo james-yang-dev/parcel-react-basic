4d2f7d9781604d12a5013b674de9332289621f98
carlos-yang <carlos.yang@kakaocorp.com>
2020-07-13 14:56:33 +0900

    parcel-react 초기화 커밋

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App(props) {
+  console.log('app js')
+  return (
+    <div>
+      <div>Welcome react app</div>
+      <link rel="stylesheet" type="text/css" href="./test.css" />
+    </div>
+  )
+}
+
+export default App
